7d12f21ac994846c50e5af10a421a9e5ee49bf5e
Mehazava <46211177+Mehazava@users.noreply.github.com>
2019-01-15 22:57:10 +1000

    Add files via upload

--- CoolStuff/backup/unit1.pas
+++ CoolStuff/backup/unit1.pas
@@ -25,10 +25,11 @@ type
     AccountCB: TComboBox;
     Label1: TLabel;
     Label2: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
+    StandName: TLabel;
+    StandPrice: TLabel;
     GoodsList: TListBox;
     Panel1: TPanel;
+    procedure FormCreate(Sender: TObject);
     procedure Panel1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
   private
@@ -47,13 +48,14 @@ type
     Owner, Name: String;
     Price, GoT: Int64;
     Goods: array of TGoods;
+    constructor Create();
   end;
 
 var
   Form1: TForm1;
   StandArr: array[0..STALLNUM] of TStand;
   StandPos: array[0..STALLNUM, 0..1] of Integer;
-  StandSizeX, StandSizeY: Integer;
+  StandSizeX, StandSizeY, ActiveStand, SCounter: Integer;
 
 implementation
 
@@ -61,16 +63,51 @@ implementation
 
 { TForm1 }
 
+constructor TStand.Create();
+begin
+  SCounter += 1;
+  Name := 'Shop ' + IntToStr(SCounter);
+  Price := SCounter * 100;
+  Owner := 'NULL';
+  GoT := -1;
+  Form1.StandName.Caption := 'asd';
+end;
+
+procedure Choose(N: Integer);
+var
+  i: Integer;
+begin
+  ActiveStand := N;
+  Form1.StandName.Caption := StandArr[N].Name;
+  Form1.StandPrice.Caption := IntToStr(StandArr[N].Price) + ' $';
+  if (StandArr[N].Owner = Form1.AccountCB.Caption) then
+    Form1.BuyStandButton.Caption := 'Продать киосок'
+  else Form1.BuyStandButton.Caption := 'Купить киосок';
+  for i := 0 to StandArr[N].GoT do begin
+
+  end;
+end;
+
 procedure TForm1.Panel1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 var
-  i, x, y: Integer;
+  i: Integer;
 begin
   for i := 0 to STALLNUM do begin
-
+    //if ((X > StandPos[i, 0]) and (X < StandPos[i, 0] + StandSizeX) and
+    //  (Y > StandPos[i, 1]) and (Y < StandPos[i, 1] + StandSizeY))
+    //then Choose(i);
   end;
-  Form1.Funds.Caption := IntToStr(X);
+  Choose(3);//
 end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  i: Integer;
+begin
+  SCounter := 0;
+end;
+
 
 end.
 
